be56d97eec8de5cc740efe250ce9c18a48f7ae50
Udara Dananjaya <b.udara.dananjaya.dev@gmail.com>
2024-06-24 13:27:25 +0530

    Create StudentModel.cs

---
+++ Sample_Dot_Net/Models/StudentModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sample_Dot_Net.Models
+{
+    [Table("student")]
+    public class StudentModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+        [Required]
+        public string first_name { get; set; }
+
+        [Required]
+        public string last_name { get; set;}
+
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string contact_number { get; set; }
+
+    }
+}
